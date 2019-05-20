964e073f2ad655559d68a483b6d625e6a6c613ff
Joseph Brown <jbrown3859@gmail.com>
2019-05-20 16:09:04 -0700

    base files

--- GameState.java
+++ GameState.java
@@ -2,6 +2,7 @@ public interface GameState
 {
 	boolean isGameOver();
 	Player getWinner();
+	Player getCurrentPlayer();
 	ArrayList<String> getCurrentMoves();
 	void makeMove(String move);
 	String toString();
